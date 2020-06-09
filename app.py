63dbd8828d1f55ca6cc9d1d0419c5db83a6806a8
Sohail <sohailshamas@yahoo.com>
2020-06-09 08:20:43 +0000

    Added envirnment variable and .gitignore file

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import os
+if os.path.exists("env.py"):
+    import env
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGODB_URI"] = os.getenv("MONGO_URI")
+
 
 
 @app.route('/')
