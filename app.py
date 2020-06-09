afbb805f615081d54b2d59888b8c4653340c2671
Sohail <sohailshamas@yahoo.com>
2020-06-09 10:10:58 +0000

    Added Materialize CDNs in base.html

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 import os
 if os.path.exists("env.py"):
     import env
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGODB_URI"] = os.getenv("MONGO_URI")
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
-
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
