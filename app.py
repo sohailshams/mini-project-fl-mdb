c8a7350bb03b81107cd03024341e77392bcaa90e
Sohail <sohailshamas@yahoo.com>
2020-06-10 21:27:12 +0000

    added update and delete category functions

--- app.py
+++ app.py
@@ -64,6 +64,27 @@ def get_categories():
     return render_template('categories.html',
                            catagories=mongo.db.catagories.find())
 
+
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+                           category=mongo.db.catagories.find_one(
+                             {'_id': ObjectId(category_id)}))
+
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.catagories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.catagories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
