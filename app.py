9970d2940e8477346fe4f44a793ce4162e24f919
Sohail <sohailshamas@yahoo.com>
2020-06-11 08:13:26 +0000

    addded add category function

--- app.py
+++ app.py
@@ -85,6 +85,18 @@ def delete_category(category_id):
     mongo.db.catagories.remove({'_id': ObjectId(category_id)})
     return redirect(url_for('get_categories'))
 
+
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    category_doc = {'category_name': request.form.get('category_name')}
+    mongo.db.catagories.insert_one(category_doc)
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/add_category')
+def add_category():
+    return render_template('addcategory.html')
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
